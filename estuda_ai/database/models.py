cbff38c39438c8df430dd64b005ee5c6308a2531
Rogerio Campos <rogerio.campos07@gmail.com>
2025-05-16 12:43:21 -0300

    alteração de tipo na tabela

--- estuda_ai/database/models.py
+++ estuda_ai/database/models.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date, datetime, time
 
 from sqlmodel import Field, SQLModel
 
@@ -7,9 +7,9 @@ class Curso(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     nome: str
     ativo: bool = Field(default=True)
-    data_criacao: str
-    data_atualizacao: str
-    total_horas: str
+    data_criacao: date
+    data_atualizacao: date
+    total_horas: time
 
     def __repr__(self):
         return f'Curso {self.nome}'
@@ -24,7 +24,7 @@ class Aula(SQLModel, table=True):
     data: date
     inicio: datetime
     fim: datetime
-    total_horas: str
+    total_horas: time
 
     def __repr__(self):
         return f'Aula: {self.nome}'
