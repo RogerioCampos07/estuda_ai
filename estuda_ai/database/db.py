8f3df6a6ea0b13037db7c5d4399fed85d0d842ef
Rogerio Campos <rogerio.campos07@gmail.com>
2025-05-16 12:01:02 -0300

    transferida a DATABASE_URL para o arquivo .env

--- estuda_ai/database/db.py
+++ estuda_ai/database/db.py
@@ -1,10 +1,19 @@
 from sqlmodel import create_engine, SQLModel
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-sql_url = "sqlite:///./database.db"
-engine = create_engine(sql_url, echo=True)
+sql_url = os.getenv('DATABASE_URL')
+engine = create_engine(sql_url)
 
 
 def create_db_and_tables(engine):
     """Create the database and tables."""
     SQLModel.metadata.create_all(engine)
+
+
+
+
+
+
