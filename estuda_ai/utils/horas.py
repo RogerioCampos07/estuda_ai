6b74d77a06d9ef24a9d1ad9daa4372e6b5b89d1e
Rogerio Campos <rogerio.campos07@gmail.com>
2025-05-16 12:44:33 -0300

    estudos e teste com a lib datetime

--- estuda_ai/utils/horas.py
+++ estuda_ai/utils/horas.py
@@ -4,8 +4,12 @@ from datetime import datetime, date
 def pega_hora():
     hoje = datetime.now()
     hora = hoje.time()
-    hora_formatada = hora.strftime("%H:%M:%S")
+    hora_formatada = hora.strftime('%H:%M:%S')
     return hora_formatada
+
+
+data_hoje = date.today()
+hora_atual = datetime.now().time()
 
 
 
