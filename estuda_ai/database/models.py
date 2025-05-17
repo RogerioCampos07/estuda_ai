f955e0a17cdfd56615cabd8e3cf29ec883c0c188
Rogerio Campos <rogerio.campos07@gmail.com>
2025-05-17 14:27:06 -0300

    alteração tabela Aula e inclusão da tabela Estudo

--- estuda_ai/database/models.py
+++ estuda_ai/database/models.py
@@ -12,17 +12,17 @@ class Curso(SQLModel, table=True):
     total_horas: time
 
     def __repr__(self):
-        return f'Curso {self.nome}'
+        return self.nome
 
 
 class Aula(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     curso_id: int = Field(foreign_key='curso.id')
     nome: str
-    total_horas: time
-
+    
     def __repr__(self):
-        return f'Aula: {self.nome}'
+        return self.nome
+
     
 
 class Estudo(SQLModel, table=True):
@@ -34,8 +34,7 @@ class Estudo(SQLModel, table=True):
     fim: datetime
     horas_estudadas: time
 
-    def __repr__(self):
-        return f'Estudo {self.data_estudo} - {self.horas_estudadas}'
+    
     
 
 
